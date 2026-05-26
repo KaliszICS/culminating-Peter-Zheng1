f901e0d17e16252a38a70103b21828ddf60838f0
Peter-Zheng1 <343277125@tdsb.ca>
2026-05-26 21:01:43 +0000

    done, imported everything

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,28 @@
-public class PracticeProblem {
+/**
+
+        * File: Culminating Assignment | Battleship
+
+        * Author: Peter Zheng
+
+        * Date Created: May 26, 2026
+
+        * Date Last Modified: May 26, 2026
+
+        */
+
+import javafx.application.application;
+import javafx.scene.Scene;
+import javafx.scene.control.button;
+import javafx.scene.layout.gridpane;
+import javafx.stage.stage;
+
+import java.util.random;
+
+public class PracticeProblem extends appication{
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
 
 }
