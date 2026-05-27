57c2868b460167fcb3db39222fcd3c8581ea12ad
Peter-Zheng1 <343277125@tdsb.ca>
2026-05-27 22:21:10 +0000

    finished varibles + part of game set up

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,6 +10,8 @@
 
         */
 
+
+// importing javafx, might add more - temp comment
 import javafx.application.application;
 import javafx.scene.Scene;
 import javafx.scene.control.button;
@@ -18,11 +20,49 @@ import javafx.stage.stage;
 
 import java.util.random;
 
-public class PracticeProblem extends appication{
+public class PracticeProblem extends application{
 
+
+        // Variables
+        int SIZE = 5;
+
+        char[][] playerBoard;
+        char[][] enemyBoard;
+
+        Button[][] playerButtons;
+        Button[][] enemyButtons;
+
+        boolean playerTurn;
+        boolean setupMode;
+
+        int shipsPlaced;
+
+        Random rand;
+        Label title;
+        GridPane grid;
+
+        //starts javafx
 	public static void main(String args[]) {
 
+                launch(args);
 	}
 
+        // Start (Game Setup)
+        @Override
+        public void start(Stage stage){
+
+        playerBoard = new char[SIZE][SIZE];
+        enemyBoard = new char[SIZE][SIZE];
+
+        playerButtons = new Button[SIZE][SIZE];
+        enermyButtons = new Button[SIZE][SIZE];
+
+        rand = new Random();
+
+        setupMode = true;
+        playerTurn = true;
+
+        shipsPlaced = 0;
+        }
 
 }
